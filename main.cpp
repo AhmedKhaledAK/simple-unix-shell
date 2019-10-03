6c5ddfac0c9567b0fb94120a6b85b26adb84c2a7
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-03 16:21:19 +0200

    getting familiar with fork() system call

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x = 1;
+    int process = fork();
+
+    if (process == 0){
+        cout << "Child ID: " << getpid() << "\n";
+        cout << "Parent ID: " << getppid() << "\n";
+        printf("Child has x = %d\n", ++x);
+    }
+    else {
+        cout << "Process ID: " << getpid() << "\n";
+        cout << "Parent ID: " << getppid() << "\n";
+        printf("Parent has x = %d\n", --x);
+    }
     return 0;
 }
