5451afbcf4809d3e67ac3a8728a883de7ca883d2
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-10-13 23:25:32 +0200

    writing to file when child process terminates

--- main.cpp
+++ main.cpp
@@ -22,27 +22,38 @@ int tokenize_command(char * ar [], char str []){
     return i;
 }
 
-void sig_handler(int iSignal)
-{
-    //signal(SIGCHLD,sig_handler); //reset handler to catch SIGCHLD for next time;
-    int status;
-    pid_t pid;
-    //if(!bg)
-    pid = wait(&status); //After wait, child is definitely freed.
-    printf("pid = %d , status = %d\n", pid, status);
-    if(WIFEXITED(status)) printf("child exited with = %d\n",WEXITSTATUS(status));
-}
-
 void write_to_file(pid_t pid, int status){
     ofstream myfile;
     myfile.open("logs.txt", ios_base::app);
-    myfile << "Child process with id: " << pid << " terminated with status: " << status << "\n";
+    myfile << "Child process with id: " << "" << " terminated with status: " << "" << "\n";
     myfile.close();
+}
+
+void sig_handler(int iSignal)
+{
+    //int status;
+    //pid_t pid = wait(&status);
+
+    write_to_file(0,0);
+
+    /*if(WIFEXITED(status)) {
+        printf("child exited with = %d and %d\n",WEXITSTATUS(status), pid);
+        write_to_file(pid, WEXITSTATUS(status));
+    }*/
+    /*int status;
+    pid_t pid;
+    cout << "pid: " << getpid() << "\n";
+    pid = wait(&status); //After wait, child is definitely freed.
+    printf("pid = %d , status = %d\n", pid, status);
+    if(WIFEXITED(status)) {
+        printf("child exited with = %d and %d\n",WEXITSTATUS(status), pid);
+        write_to_file(pid, WEXITSTATUS(status));
+    }*/
 }
 
 void execute_command(char * ar[], bool bg){
 
-    //signal(SIGCHLD,sig_handler);
+    signal(SIGCHLD,sig_handler);
     pid_t process = fork();
 
     if (process== 0){
@@ -55,12 +66,6 @@ void execute_command(char * ar[], bool bg){
         printf("hello from parent: %d\n", getpid());
         int status;
         if(!bg) waitpid(process,&status,0);
-        //wait(NULL);
-        if(WIFEXITED(status)) {
-            printf("child exited with = %d and %d\n",WEXITSTATUS(status), process);
-            write_to_file(process, WEXITSTATUS(status));
-        }
-
         printf("child has terminated\n");
     }
 
